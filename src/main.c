a25fee44de6b887e5997eab31a96ce7c73d387e9
Mkac003 <mkac003@gmail.com>
2024-05-20 19:06:18 +0200

    Added dragging, proper layout and rendering

--- src/main.c
+++ src/main.c
@@ -22,6 +22,8 @@ void Blit(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y) {
   if (SDL_RenderCopy(renderer, texture, NULL, &(SDL_Rect) {x, y, w, h})) handle_sdl_error();
   }
 
+#define ASSERT(x, msg) do {if (!(x)) {printf("(%s:%d) assertion %s failed: %s\n", __FILE__, __LINE__, #x, msg);}} while (0)
+
 /* ========== MAIN ========== */
 
 /* indexing macros */
@@ -44,6 +46,9 @@ void Blit(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y) {
 
 #define SELECTION_SIZE 3
 
+#define NOT_DRAGGING -1
+#define MOUSE_DRAG_PADDING 20
+
 /* Shapes */
 
 typedef struct {
@@ -58,6 +63,21 @@ const Shape shape_templates[] = {
     2, 2,
     0,
     "1111",
+    },
+  (Shape) {
+    3, 2,
+    0,
+    "111111",
+    },
+  (Shape) {
+    2, 3,
+    0,
+    "111111",
+    },
+  (Shape) {
+    3, 3,
+    0,
+    "111111111",
     },
   };
 
@@ -92,6 +112,7 @@ typedef struct {
   uint8_t board[BOARD_SIZE * BOARD_SIZE];
   
   Shape selection[SELECTION_SIZE];
+  int dragging_shape;
   } GameContext;
 
 void init(GameContext *ctx) {
@@ -120,6 +141,9 @@ void init(GameContext *ctx) {
   for (int i=0; i<SELECTION_SIZE; ++i) {
     ctx->selection[i] = shape_from_template(randint(0, NUM_TEMPLATES-1), randint(1, NUM_COLORS-1));
     }
+  
+  /* no shape is currently being dragged */
+  ctx->dragging_shape = NOT_DRAGGING;
   }
 
 void draw_block(GameContext *ctx, int screen_x, int screen_y, SDL_Color color) {
@@ -128,7 +152,7 @@ void draw_block(GameContext *ctx, int screen_x, int screen_y, SDL_Color color) {
   
   SDL_Texture *texture = ctx->textures[TEXTURE_BLOCK];
   
-  SDL_SetTextureAlphaMod(texture, 128);
+  SDL_SetTextureAlphaMod(texture, 96);
   SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
   Blit(ctx->renderer, texture, screen_x, screen_y);
   }
@@ -136,23 +160,54 @@ void draw_block(GameContext *ctx, int screen_x, int screen_y, SDL_Color color) {
 void draw_shape(GameContext *ctx, Shape shape, int screen_x, int screen_y) {
   for (int block_x=0; block_x<shape.width; ++block_x) {
     for (int block_y=0; block_y<shape.height; ++block_y) {
-      if (shape.data[block_y * shape.height + block_x] != '1') continue;
+      if (shape.data[block_y * shape.width + block_x] != '1') continue;
       
       draw_block(ctx, block_x * BLOCK_SIZE_PX + screen_x, block_y * BLOCK_SIZE_PX + screen_y, colors[shape.color]);
       }
     }
   }
 
+bool shape_is_hovered(Shape shape, int screen_x, int screen_y, int mouse_x, int mouse_y) {
+  int width_px = shape.width * BLOCK_SIZE_PX;
+  int height_px = shape.height * BLOCK_SIZE_PX;
+  
+  /* Rectangle check */
+  if (!(mouse_x > screen_x && mouse_x < screen_x + width_px)
+   || !(mouse_y > screen_y && mouse_y < screen_y + height_px))
+    return false;
+  
+  /* Precise check */
+  int block_x = floor((float) (mouse_x - screen_x) / (float) BLOCK_SIZE_PX);
+  int block_y = floor((float) (mouse_y - screen_y) / (float) BLOCK_SIZE_PX);
+  
+  ASSERT(block_x >= 0, "block_x !>= 0");
+  ASSERT(block_y >= 0, "block_y !>= 0");
+  ASSERT(block_x < shape.width, "block_x > shape.width");
+  ASSERT(block_y < shape.height, "block_y > shape.height");
+  
+  if (shape.data[block_y * shape.width + block_x] != '1')
+    return false;
+  
+  return true;
+  }
+
 bool frame(GameContext *ctx) {
+  bool just_clicked = false;
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
     if (event.type == SDL_QUIT) return true;
+    if (event.type == SDL_MOUSEBUTTONDOWN) {
+      if (event.button.button == SDL_BUTTON_LEFT) just_clicked = true;
+      }
     }
   
   int board_position[2] = {
     ctx->window_size[WIDTH] / 2 - (BLOCK_SIZE_PX * BOARD_SIZE) / 2,
     100
     };
+  
+  int mouse_position[2] = {0};
+  uint32_t button_mask = SDL_GetMouseState(&mouse_position[X], &mouse_position[Y]);
   
   SDL_SetRenderDrawColor(ctx->renderer, 0, 0, 0, 255);
   SDL_RenderClear(ctx->renderer);
@@ -167,7 +222,11 @@ bool frame(GameContext *ctx) {
       }
     }
   
-  /* Draw the selection */
+  /* Release the dragged object if the mouse is no longer held */
+  if (!(button_mask & SDL_BUTTON(1)))
+    ctx->dragging_shape = NOT_DRAGGING;
+  
+  /* Draw and update the selection */
   {
     const int padding = 100;
     int center_x, screen_x;
@@ -176,10 +235,25 @@ bool frame(GameContext *ctx) {
     for (int i=0; i<SELECTION_SIZE; ++i) {
       shape = ctx->selection[i];
       
-      center_x = (ctx->window_size[WIDTH] / 2) - (SELECTION_SIZE * padding / 2) + (i * padding) + padding / 2;
-      screen_x = center_x - shape.width * BLOCK_SIZE_PX / 2;
-      
-      draw_shape(ctx, shape, screen_x, board_position[Y] + BOARD_SIZE * BLOCK_SIZE_PX + 10);
+      if (ctx->dragging_shape == i) {
+        screen_x = mouse_position[X] - shape.width * BLOCK_SIZE_PX / 2;
+        screen_y = mouse_position[Y] - shape.height * BLOCK_SIZE_PX - MOUSE_DRAG_PADDING;
+        
+        draw_shape(ctx, shape, screen_x, screen_y);
+        }
+      else {
+        center_x = (ctx->window_size[WIDTH] / 2) - (SELECTION_SIZE * padding / 2) + (i * padding) + padding / 2;
+        screen_x = center_x - shape.width * BLOCK_SIZE_PX / 2;
+        screen_y = board_position[Y] + BOARD_SIZE * BLOCK_SIZE_PX + 10;
+        
+        draw_shape(ctx, shape, screen_x, screen_y);
+        
+        if (shape_is_hovered(shape, screen_x, screen_y, mouse_position[X], mouse_position[Y])) {
+          if (just_clicked) {
+            ctx->dragging_shape = i;
+            }
+          }
+        }
       }
     }
   
